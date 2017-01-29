4c331e2c551dc3b0d6503a7e452daecb6c044318
ShanWijenayaka <shanwijenayaka@outlook.com>
2017-01-29 17:34:39 +0530

    #########

--- src/Insertion_Sort/Main.java
+++ src/Insertion_Sort/Main.java
@@ -18,19 +18,16 @@ public class Main {
         }
     }
 
-    public int[] iSort(int [] arr) {
-        int flag = arr[0];
+    public int [] iSort(int [] arr){
 
-        for (int i = 0; i < arr.length; i++) {
-            flag = i;
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[flag] > arr[j]) {
-                    int tmp = arr[flag];
-                    arr[flag] = arr[j];
-                    arr[j] = tmp;
-                }
+        for(int i =1; i<arr.length;i++){
+            int j = i;
+            while (j>0 && arr[j-1] > arr[j]){
+                int tmp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] =tmp;
+                j = j-1;
             }
-
         }
         return arr;
     }
--- src/Selection_Sort/Main.java
+++ src/Selection_Sort/Main.java
@@ -4,4 +4,34 @@ package Selection_Sort;
  * Created by shan on 1/29/17.
  */
 public class Main {
+    int [] arr = {124,234,432,788,56,655777,78,56,45433,4};
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        for(int i: m.arr){
+            System.out.print(i+", ");
+        }
+        System.out.println();
+        m.arr = m.sSort(m.arr);
+        for(int i: m.arr){
+            System.out.print(i+", ");
+        }
+    }
+
+    public int[] sSort(int [] arr) {
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            min = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    int tmp = arr[min];
+                    arr[min] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+
+        }
+        return arr;
+    }
 }
